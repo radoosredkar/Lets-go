fc5974ecb7c2e79503db8c3a619a6aa2b0871e1b
rado <radoosredkar@hotmail.com>
2023-12-27 19:07:20 +0100

    Add post & query param

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -16,11 +17,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetView handler function.
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Display a specific snippet... with ID " + strconv.Itoa(id)))
 }
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		/*
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("405 - Method Not Allowed"))
+		*/
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		log.Print("405 - Method Not Allowed")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
 	w.Write([]byte("Create a new snippet..."))
 }
 func main() {
